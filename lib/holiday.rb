45e52f3fae39f6584098c7d350d2e483fdf19344
Alison Williams <apw5011@gmail.com>
2020-11-16 00:29:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,9 +22,6 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
   holiday_hash.each do |season, seasonal_holiday_hash|
     if season == :winter
       seasonal_holiday_hash.each do |holiday, supplies|
@@ -38,8 +35,6 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
   holiday_hash.each do |season, seasonal_holiday_hash|
     if season == :spring
       seasonal_holiday_hash.each do |holiday, supplies|
@@ -52,8 +47,6 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
   holiday_hash[season] = {holiday_name => supply_array}
 holiday_hash
 end
