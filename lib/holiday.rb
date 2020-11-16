799f93baa1933cc6875a313f68e824cf8aa37c53
Alison Williams <apw5011@gmail.com>
2020-11-16 01:39:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,10 +79,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.collect do |season, seasonal_holiday_hash|
+  holiday_hash.each do |season, seasonal_holiday_hash|
     seasonal_holiday_hash.each do |holiday, supplies|
+      bbq_array = []
       if supplies.include?("BBQ")
-        holiday
+        bbq_array << holiday
       end 
     end 
   end 
